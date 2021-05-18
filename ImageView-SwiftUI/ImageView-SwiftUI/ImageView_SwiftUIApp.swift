f3a61ce0c1c17bc5f4d84d76b9cbca7aa06307ca
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-05-18 18:08:08 +0900

    初回コミット

---
+++ ImageView-SwiftUI/ImageView-SwiftUI/ImageView_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageView_SwiftUIApp.swift
+//  ImageView-SwiftUI
+//
+//  Created by home on 2021/05/18.
+//
+
+import SwiftUI
+
+@main
+struct ImageView_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
