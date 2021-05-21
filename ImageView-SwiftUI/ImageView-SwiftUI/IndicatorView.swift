af428c7d13c0d78787d0ef4b51d8e90fc5947b32
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-05-21 20:25:50 +0900

    インジケータのViewを追加

--- ImageView-SwiftUI/ImageView-SwiftUI/IndicatorView.swift
+++ ImageView-SwiftUI/ImageView-SwiftUI/IndicatorView.swift
@@ -5,4 +5,18 @@
 //  Created by home on 2021/05/19.
 //
 
-import Foundation
+import SwiftUI
+
+struct IndicatorView: UIViewRepresentable {
+    let isAnimating: Bool
+    
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        let indicatorView = UIActivityIndicatorView(style: .medium)
+        indicatorView.hidesWhenStopped = true
+        return indicatorView
+    }
+    
+    func updateUIView(_ indicatorView: UIActivityIndicatorView, context: Context) {
+        isAnimating ? indicatorView.startAnimating() : indicatorView.stopAnimating()
+    }
+}
